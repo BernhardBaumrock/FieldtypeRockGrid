979f9c0df0f4c78934365c01b33ec953c09a8654
Bernhard Baumrock <bernhard@baumrock.com>
2018-08-18 15:30:02 +0200

    move to rockgrid

--- plugins/columns/rgColAddIcons.js
+++ plugins/columns/rgColAddIcons.js
@@ -1,29 +1,11 @@
 /**
  * add clickable icon to current cell
  * @param {object} col 
- * @param {object} params, see possible options and defaults in the code
+ * @param {array} items, array of icon-configs
  */
 var rgColAddIcons = function(col, items) {
   // get the current cellRenderer where we prepend/append the icon
   var renderer = col.cellRenderer || function(params) { return params.value; };
-
-  // replace tags by value of the current row
-  var replaceTags = function(str, params) {
-    var result = str.match(/{(.*?)}/g);
-    if(!result) return str;
-
-    // replace tags
-    var notfound = false;
-    result.map(function(val) {
-      var field = val.replace(/{|}/g,'');
-      str = str.replace(val, params.data[field]);
-      if(params.data[field] == null) notfound = true;
-    });
-
-    // don't show icon if a tag's value was null
-    if(notfound) return false;
-    return str;
-  }
 
   // apply prepend and append markup
   col.cellRenderer = function(params) {
@@ -38,14 +20,14 @@ var rgColAddIcons = function(col, items) {
       str = '';
   
       // setup parameters
-      var icon = item.icon || 'search';                             // icon
-      var type = item.type || 'after';                              // show icon before or after the content
-      var href = replaceTags(item.href || '#', params);             // href of the link
-      var show = item.show || 'always';                             // show always or only on hover
-      var dataHref = replaceTags(item.dataHref || '', params);      // data-href attribute for pw-panels
-      var target = item.target || '_blank';                         // link target
-      var label = item.label || icon;                               // link label
-      var cls = item.cls || '';                                     // link class
+      var icon = item.icon || 'search';                                   // icon
+      var type = item.type || 'after';                                    // show icon before or after the content
+      var href = RockGrid.replaceTags(item.href || '#', params);          // href of the link
+      var show = item.show || 'always';                                   // show always or only on hover
+      var dataHref = RockGrid.replaceTags(item.dataHref || '', params);   // data-href attribute for pw-panels
+      var target = item.target || '_blank';                               // link target
+      var label = item.label || icon;                                     // link label
+      var cls = RockGrid.replaceTags(item.cls || '', params);             // link class
 
       // early exit if a tag had no value
       if(href === false || dataHref === false) continue;
