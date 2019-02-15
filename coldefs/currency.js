5361c86741844b42a1dc222d513bcf013ffc4429
Bernhard Baumrock <bernhard@baumrock.com>
2019-02-15 13:10:20 +0100

    add support for custom valuegetters

--- coldefs/currency.js
+++ coldefs/currency.js
@@ -2,12 +2,18 @@ document.addEventListener('RockGridReady', function(e) {
   RockGrid.colDefs.currency = function(colDef, params) {
     var params = params || {};
 
-    // set coldefs
+    // old valuegetter
+    var valueGetter = colDef.valueGetter || false;
+
     colDef.valueGetter = function(params) {
+      // if a valuegetter is already set we take this one
+      if(valueGetter) return valueGetter(params)*1;
+
       if(typeof params.data == 'undefined') return;
       var val = params.data[colDef.field];
       return val*1; // make sure the value is a number and not a string
     }
+
     colDef.type = 'numericColumn';
     colDef.cellRenderer = RockGrid.renderers.currency;
     colDef.cellRendererParams = {
