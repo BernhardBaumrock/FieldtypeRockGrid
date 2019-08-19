f9a3a5a66145052a5dc5118b694529156bab9907
BernhardBaumrock <office@baumrock.com>
2019-08-19 15:17:58 +0200

    bugfix

--- plugins/tippy.js
+++ plugins/tippy.js
@@ -2,12 +2,13 @@
 $(document).on('hover', '.rg-tippy', function(e) {
   var $el = $(e.target).closest('.rg-tippy');
   var $pop = $el.next('.rg-tippy-hidden');
+
   tippy($el[0], {
     theme: 'light-border',
     content: $pop.html(),
     interactive: true,
     arrow: true,
-    placement: $el.data('placement'),
+    placement: $el.data('placement') || 'top',
     zIndex: 999, // pw-panels have 1000
   });
 });
